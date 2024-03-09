da7e19673d7903777cc50d8b2de4bae9c97e7fc5
Dharm Raj <37910932+dharm98688@users.noreply.github.com>
2024-03-10 03:57:24 +0530

    Update info.py

--- LoginSystem/info.py
+++ LoginSystem/info.py
@@ -2,5 +2,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'dharmanu551995@gmail.com'
-EMAIL_HOST_PASSWORD = 'xwrr cnor dlrf xttk'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
